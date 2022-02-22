9e90824c18c7b3c418e7d56c382bf9c6502c93fa
Juarez <a1016060@bestbuy.com>
2022-02-22 09:16:08 -0600

    day16.2

---
+++ src/com/iAmLuisJ/Day16.java
@@ -0,0 +1,20 @@
+package com.iAmLuisJ;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day16 {
+    public static void main(String[] args) throws FileNotFoundException {
+        //Day 16
+        //parse input
+        File myFile = new File("C:\\Users\\a1016060\\GitHub\\AoC 2021\\aoc\\data\\Day16\\input.txt");
+        Scanner myScan = new Scanner(myFile);
+        String input = myScan.nextLine();
+        myScan.close();
+        //convert hexadecimal representation to binary
+        //each character of hexadecimal corresponds to four bits of binary
+
+        
+    }
+}
